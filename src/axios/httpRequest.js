464e7c6f82fcbe300eb95284d032c73cbe40cf8e
soyedpork27 <chan4728@kakao.com>
2024-09-08 11:05:38 +0900

    MD

--- src/axios/httpRequest.js
+++ src/axios/httpRequest.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+
 export const httpRequest = axios.create({
   baseURL : 'http://localhost:9812',
+  // baseURL : `${process.env.REACT_APP_API_BASE_URL}`,
   headers : {
     'Content-Type': 'application/json'
   }
